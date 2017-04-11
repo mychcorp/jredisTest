51da1600299b57fd2185d2156c83480f3573ac69
376490270@qq.com <jzc512623>
2017-04-11 10:23:29 +0800

    change some data

--- src/main/java/com/mychcorp/RedisUtil.java
+++ src/main/java/com/mychcorp/RedisUtil.java
@@ -16,7 +16,7 @@ public final class RedisUtil {
     private static int PORT = 6379;
 
     //访问密码
-    private static String AUTH = "root";
+    private static String AUTH = "192.168.0.100";
 
     //可用连接实例的最大数目，默认值为8；
     //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
